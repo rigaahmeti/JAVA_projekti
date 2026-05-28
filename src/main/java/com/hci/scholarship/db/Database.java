6bdc0dd404b1fd1d1f1f924b9b30bbedbe1dc71c
Rinesa53 <rinesa.kurtaliqi@student.uni-pr.edu>
2026-05-28 17:37:08 +0200

    Shtova metodën për regjistrimin e përdoruesve me fjalëkalim të hash-uar

--- src/main/java/com/hci/scholarship/db/Database.java
+++ src/main/java/com/hci/scholarship/db/Database.java
@@ -79,4 +79,11 @@ public class Database {
         }
     }
 
-
+    private static void addUser(java.sql.PreparedStatement ps, String username, String password, String fullName, String role)
+            throws SQLException {
+        ps.setString(1, username);
+        ps.setString(2, UserRepository.hash(password));
+        ps.setString(3, fullName);
+        ps.setString(4, role);
+        ps.executeUpdate();
+    }
