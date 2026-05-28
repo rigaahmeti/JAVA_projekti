43be8eded170286c1c9514d0c994070256917b14
Rinesa53 <rinesa.kurtaliqi@student.uni-pr.edu>
2026-05-28 19:29:06 +0200

    repository

--- src/main/java/com/hci/scholarship/repository/ApplicationRepository.java
+++ src/main/java/com/hci/scholarship/repository/ApplicationRepository.java
@@ -1,5 +1,10 @@
 ﻿package com.hci.scholarship.repository;
 
-public class ApplicationRepository {
-}
+import com.hci.scholarship.db.Database;
+import com.hci.scholarship.model.ScholarshipApplication;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApplicationRepository {
