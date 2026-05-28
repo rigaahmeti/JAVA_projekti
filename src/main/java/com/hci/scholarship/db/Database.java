b07a040410e74f37c52bb2b596455e5fe7394724
Rinesa53 <rinesa.kurtaliqi@student.uni-pr.edu>
2026-05-28 17:06:31 +0200

    filiimi i krijimit te databazes

--- src/main/java/com/hci/scholarship/db/Database.java
+++ src/main/java/com/hci/scholarship/db/Database.java
@@ -1,5 +1,22 @@
 ﻿package com.hci.scholarship.db;
 
+import com.hci.scholarship.repository.UserRepository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
 public class Database {
-}
+    private static final String URL = "jdbc:sqlite:scholarships.db";
+
+    public static String url() {
+        return URL;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+
 
