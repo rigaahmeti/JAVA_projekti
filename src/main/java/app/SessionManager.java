ba7550452decfd473c33bbc1b81e85947664a94b
arsaduraku <arsa.duraku@student.uni-pr.edu>
2026-05-28 17:04:32 +0200

    U shtua SessionManager file

--- src/main/java/app/SessionManager.java
+++ src/main/java/app/SessionManager.java
@@ -1,5 +1,27 @@
 ﻿package app;
 
 public class SessionManager {
+    private static String currentUser;
+    private static String currentRole;
+
+    private SessionManager() {}
+
+    public static void login(String username, String role) {
+        currentUser = username;
+        currentRole = role;
+    }
+
+    public static void logout() {
+        currentUser = null;
+        currentRole = null;
+    }
+
+    public static String getCurrentUser() {
+        return currentUser;
+    }
+
+    public static String getCurrentRole() {
+        return currentRole;
+    }
 }
 
