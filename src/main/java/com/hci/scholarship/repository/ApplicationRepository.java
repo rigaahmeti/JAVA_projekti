9b58d78fb48cad079623fc2eab30637f3913f223
Rinesa53 <rinesa.kurtaliqi@student.uni-pr.edu>
2026-05-28 19:49:44 +0200

    Shtova metodën për ruajtjen e aplikimeve të bursave

--- src/main/java/com/hci/scholarship/repository/ApplicationRepository.java
+++ src/main/java/com/hci/scholarship/repository/ApplicationRepository.java
@@ -8,3 +8,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ApplicationRepository {
+    public void save(ScholarshipApplication app) throws SQLException {
+        String sql = """
+                INSERT INTO scholarship_applications
+                (student_name, index_number, faculty, study_program, study_year, average_grade, family_income,
+                 scholarship_type, email, phone, municipality, ects_credits, household_members, special_category,
+                 scholarship_cycle, motivation, document_summary, status, gender, documents_confirmed, ai_recommendation, ai_score)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                """;
